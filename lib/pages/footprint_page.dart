f981d7497167a64f41b41c17f55b97bec85c3619
Arunraj v <120666350+Arunrxj-v@users.noreply.github.com>
2024-08-22 20:28:55 +0530

    veendum changes

--- lib/pages/footprint_page.dart
+++ lib/pages/footprint_page.dart
@@ -34,7 +34,7 @@ class FootPrint extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20, top: 40),
+          padding: const EdgeInsets.only(left: 20.0, right: 20, top: 120),
           child: ListView.separated(
             itemCount: 3,
             separatorBuilder: (_, __) => const SizedBox(height: 30),
