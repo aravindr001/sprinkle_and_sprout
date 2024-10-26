d1c75c8b46cf0ad840c9b77d71951cfe1baf2e62
Aravind R <120251962+aravindr001@users.noreply.github.com>
2024-10-26 21:39:29 +0530

    commited

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -15,4 +15,4 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 String BASE_URL = "https://api.openai.com/v1";
 
 // Retrieve the API key from the environment variable
-// String API_KEY = dotenv.env['API_KEY'] ?? "defaultAPIkeyIfNotSet";
+
