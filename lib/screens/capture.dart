52a6f413f080a2f6acd35fb0b2b365110d930115
Aravind R <120251962+aravindr001@users.noreply.github.com>
2024-10-27 14:25:33 +0530

    commit

---
+++ lib/screens/capture.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Capture extends StatelessWidget {
+
+  const Capture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
